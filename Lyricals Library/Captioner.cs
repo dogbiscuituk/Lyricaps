1596f0d81ba5c198626a96b8fc3945638d6de65d
John Kerr <john@jmkerr.com>
2021-04-24 00:30:29 +0100

    Initial commit

---
+++ Lyricals Library/Captioner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LyricapsLibrary
+{
+    public class Captioner
+    {
+        public static IEnumerable<string> Caption(IEnumerable<string> lines, int minutes, int seconds) =>
+            Caption(lines, new TimeSpan(hours: 0, minutes: minutes, seconds: seconds));
+
+        public static IEnumerable<string> Caption(IEnumerable<string> lines, TimeSpan time)
+        {
+            var output = new List<string>();
+            int
+                lineCount = lines.Count(),
+                lineNumber = 1;
+            string
+                text = string.Empty,
+                start,
+                stop = "00:00:00,000";
+            var totalTime = time.TotalMilliseconds;
+            foreach (var line in lines)
+            {
+                start = stop;
+                stop = TimeSpan.FromMilliseconds(totalTime * lineNumber / lineCount).ToString(@"hh\:mm\:ss\,fff");
+                output.Add($"{lineNumber}");
+                output.Add($"{start} --> {stop}");
+                if (!string.IsNullOrWhiteSpace(line))
+                    text = line;
+                output.Add(text);
+                output.Add(string.Empty);
+                lineNumber++;
+            }
+            return output;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Lyricaps
+ 
