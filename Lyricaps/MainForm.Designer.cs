59e93b99dbecd58f469c09d9eb43047ef4e6c2fc
John Kerr <john@jmkerr.com>
2021-04-25 00:10:10 +0100

    Add video file duration measurement.

---
+++ Lyricaps/MainForm.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Lyricaps
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LyricsTextBox = new System.Windows.Forms.TextBox();
+            this.PopupLyrics = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.PopupLyricsOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.PopupLyricsSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.PopupLyricsToggleSplit = new System.Windows.Forms.ToolStripMenuItem();
+            this.edMinutes = new System.Windows.Forms.NumericUpDown();
+            this.edSeconds = new System.Windows.Forms.NumericUpDown();
+            this.CaptionsTextBox = new System.Windows.Forms.TextBox();
+            this.PopupCaptions = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.PopupCaptionsSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.PopupCaptionsToggleSplit = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SplitContainer = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSelectVideoFile = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.edHours = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.VideoOpenDialog = new System.Windows.Forms.OpenFileDialog();
+            this.LyricsOpenDialog = new System.Windows.Forms.OpenFileDialog();
+            this.CaptionsSaveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.LyricsSaveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.PopupLyrics.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edMinutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSeconds)).BeginInit();
+            this.PopupCaptions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
+            this.SplitContainer.Panel1.SuspendLayout();
+            this.SplitContainer.Panel2.SuspendLayout();
+            this.SplitContainer.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edHours)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LyricsTextBox
+            // 
+            this.LyricsTextBox.ContextMenuStrip = this.PopupLyrics;
+            this.LyricsTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LyricsTextBox.Location = new System.Drawing.Point(0, 0);
+            this.LyricsTextBox.Multiline = true;
+            this.LyricsTextBox.Name = "LyricsTextBox";
+            this.LyricsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.LyricsTextBox.Size = new System.Drawing.Size(390, 528);
+            this.LyricsTextBox.TabIndex = 1;
+            this.LyricsTextBox.WordWrap = false;
+            this.LyricsTextBox.TextChanged += new System.EventHandler(this.edLyrics_TextChanged);
+            // 
+            // PopupLyrics
+            // 
+            this.PopupLyrics.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PopupLyricsOpen,
+            this.PopupLyricsSave,
+            this.toolStripMenuItem1,
+            this.PopupLyricsToggleSplit});
+            this.PopupLyrics.Name = "PopupLyrics";
+            this.PopupLyrics.Size = new System.Drawing.Size(145, 76);
+            // 
+            // PopupLyricsOpen
+            // 
+            this.PopupLyricsOpen.Name = "PopupLyricsOpen";
+            this.PopupLyricsOpen.Size = new System.Drawing.Size(144, 22);
+            this.PopupLyricsOpen.Text = "Open Lyrics...";
+            this.PopupLyricsOpen.Click += new System.EventHandler(this.PopupLyricsOpen_Click);
+            // 
+            // PopupLyricsSave
+            // 
+            this.PopupLyricsSave.Name = "PopupLyricsSave";
+            this.PopupLyricsSave.Size = new System.Drawing.Size(144, 22);
+            this.PopupLyricsSave.Text = "Save Lyrics...";
+            this.PopupLyricsSave.Click += new System.EventHandler(this.PopupLyricsSave_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(141, 6);
+            // 
+            // PopupLyricsToggleSplit
+            // 
+            this.PopupLyricsToggleSplit.Name = "PopupLyricsToggleSplit";
+            this.PopupLyricsToggleSplit.Size = new System.Drawing.Size(144, 22);
+            this.PopupLyricsToggleSplit.Text = "Toggle Split";
+            this.PopupLyricsToggleSplit.Click += new System.EventHandler(this.PopupToggleSplit_Click);
+            // 
+            // edMinutes
+            // 
+            this.edMinutes.Location = new System.Drawing.Point(170, 7);
+            this.edMinutes.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.edMinutes.Name = "edMinutes";
+            this.edMinutes.Size = new System.Drawing.Size(41, 20);
+            this.edMinutes.TabIndex = 2;
+            this.edMinutes.ValueChanged += new System.EventHandler(this.edVideoLength_ValueChanged);
+            // 
+            // edSeconds
+            // 
+            this.edSeconds.Location = new System.Drawing.Point(266, 7);
+            this.edSeconds.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.edSeconds.Name = "edSeconds";
+            this.edSeconds.Size = new System.Drawing.Size(41, 20);
+            this.edSeconds.TabIndex = 3;
+            this.edSeconds.ValueChanged += new System.EventHandler(this.edVideoLength_ValueChanged);
+            // 
+            // CaptionsTextBox
+            // 
+            this.CaptionsTextBox.ContextMenuStrip = this.PopupCaptions;
+            this.CaptionsTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CaptionsTextBox.Location = new System.Drawing.Point(0, 0);
+            this.CaptionsTextBox.Multiline = true;
+            this.CaptionsTextBox.Name = "CaptionsTextBox";
+            this.CaptionsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.CaptionsTextBox.Size = new System.Drawing.Size(390, 528);
+            this.CaptionsTextBox.TabIndex = 4;
+            this.CaptionsTextBox.WordWrap = false;
+            this.CaptionsTextBox.TextChanged += new System.EventHandler(this.edCaptions_TextChanged);
+            // 
+            // PopupCaptions
+            // 
+            this.PopupCaptions.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PopupCaptionsSave,
+            this.toolStripMenuItem2,
+            this.PopupCaptionsToggleSplit});
+            this.PopupCaptions.Name = "PopupCaptions";
+            this.PopupCaptions.Size = new System.Drawing.Size(158, 54);
+            // 
+            // PopupCaptionsSave
+            // 
+            this.PopupCaptionsSave.Name = "PopupCaptionsSave";
+            this.PopupCaptionsSave.Size = new System.Drawing.Size(157, 22);
+            this.PopupCaptionsSave.Text = "Save Captions...";
+            this.PopupCaptionsSave.Click += new System.EventHandler(this.PopupCaptionsSave_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(154, 6);
+            // 
+            // PopupCaptionsToggleSplit
+            // 
+            this.PopupCaptionsToggleSplit.Name = "PopupCaptionsToggleSplit";
+            this.PopupCaptionsToggleSplit.Size = new System.Drawing.Size(157, 22);
+            this.PopupCaptionsToggleSplit.Text = "Toggle Split";
+            this.PopupCaptionsToggleSplit.Click += new System.EventHandler(this.PopupToggleSplit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "seconds";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(217, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "minutes";
+            // 
+            // SplitContainer
+            // 
+            this.SplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SplitContainer.Location = new System.Drawing.Point(0, 33);
+            this.SplitContainer.Name = "SplitContainer";
+            // 
+            // SplitContainer.Panel1
+            // 
+            this.SplitContainer.Panel1.Controls.Add(this.LyricsTextBox);
+            // 
+            // SplitContainer.Panel2
+            // 
+            this.SplitContainer.Panel2.Controls.Add(this.CaptionsTextBox);
+            this.SplitContainer.Size = new System.Drawing.Size(784, 528);
+            this.SplitContainer.SplitterDistance = 390;
+            this.SplitContainer.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSelectVideoFile);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.edHours);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.edSeconds);
+            this.panel1.Controls.Add(this.edMinutes);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 33);
+            this.panel1.TabIndex = 15;
+            // 
+            // btnSelectVideoFile
+            // 
+            this.btnSelectVideoFile.Location = new System.Drawing.Point(366, 4);
+            this.btnSelectVideoFile.Name = "btnSelectVideoFile";
+            this.btnSelectVideoFile.Size = new System.Drawing.Size(117, 23);
+            this.btnSelectVideoFile.TabIndex = 17;
+            this.btnSelectVideoFile.Text = "Select video file...";
+            this.btnSelectVideoFile.UseVisualStyleBackColor = true;
+            this.btnSelectVideoFile.Click += new System.EventHandler(this.btnSelectVideoFile_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(131, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "hours";
+            // 
+            // edHours
+            // 
+            this.edHours.Location = new System.Drawing.Point(84, 7);
+            this.edHours.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.edHours.Name = "edHours";
+            this.edHours.Size = new System.Drawing.Size(41, 20);
+            this.edHours.TabIndex = 16;
+            this.edHours.ValueChanged += new System.EventHandler(this.edVideoLength_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Video length";
+            // 
+            // VideoOpenDialog
+            // 
+            this.VideoOpenDialog.Filter = "Video files (*.mov;*.mp4)|*.mov;*.mp4|Audio files (*.wav;*.mp3)|*.wav;*.mp3|All files (*.*)|*.*";
+            this.VideoOpenDialog.Title = "Select a Video File";
+            // 
+            // LyricsOpenDialog
+            // 
+            this.LyricsOpenDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.LyricsOpenDialog.Title = "Select a Lyrics File";
+            // 
+            // CaptionsSaveDialog
+            // 
+            this.CaptionsSaveDialog.DefaultExt = "srt";
+            this.CaptionsSaveDialog.Filter = "SubRip files (*.srt)|*.srt|All files (*.*)|*.*";
+            this.CaptionsSaveDialog.Title = "Save Captions File";
+            // 
+            // LyricsSaveDialog
+            // 
+            this.LyricsSaveDialog.DefaultExt = "txt";
+            this.LyricsSaveDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.LyricsSaveDialog.Title = "Save Lyrics File";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.SplitContainer);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "MainForm";
+            this.Text = "Lyric Captions";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.PopupLyrics.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.edMinutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSeconds)).EndInit();
+            this.PopupCaptions.ResumeLayout(false);
+            this.SplitContainer.Panel1.ResumeLayout(false);
+            this.SplitContainer.Panel1.PerformLayout();
+            this.SplitContainer.Panel2.ResumeLayout(false);
+            this.SplitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).EndInit();
+            this.SplitContainer.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edHours)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox LyricsTextBox;
+        private System.Windows.Forms.NumericUpDown edMinutes;
+        private System.Windows.Forms.NumericUpDown edSeconds;
+        private System.Windows.Forms.TextBox CaptionsTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.SplitContainer SplitContainer;
+        private System.Windows.Forms.ContextMenuStrip PopupLyrics;
+        private System.Windows.Forms.ToolStripMenuItem PopupLyricsOpen;
+        private System.Windows.Forms.ToolStripMenuItem PopupLyricsSave;
+        private System.Windows.Forms.ContextMenuStrip PopupCaptions;
+        private System.Windows.Forms.ToolStripMenuItem PopupCaptionsSave;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown edHours;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelectVideoFile;
+        private System.Windows.Forms.OpenFileDialog VideoOpenDialog;
+        private System.Windows.Forms.OpenFileDialog LyricsOpenDialog;
+        private System.Windows.Forms.SaveFileDialog CaptionsSaveDialog;
+        private System.Windows.Forms.SaveFileDialog LyricsSaveDialog;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem PopupLyricsToggleSplit;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem PopupCaptionsToggleSplit;
+    }
+}
+
---
+++ Lyricaps/MainForm.cs
@@ -0,0 +1,190 @@
+﻿namespace Lyricaps
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    public partial class MainForm : Form
+    {
+        #region Constructor
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Fields
+
+        private bool CaptionsUpdated, LyricsEdited;
+
+        #endregion
+
+        #region Menu Event Handlers
+
+        private void PopupCaptionsSave_Click(object sender, EventArgs e) => SaveCaptions();
+
+        private void PopupLyricsOpen_Click(object sender, EventArgs e) => LoadLyrics();
+
+        private void PopupLyricsSave_Click(object sender, EventArgs e) => SaveLyrics();
+
+        private void PopupToggleSplit_Click(object sender, EventArgs e) => ToggleSplit();
+
+        #endregion
+
+        #region Control Event Handlers
+
+        private void btnSelectVideoFile_Click(object sender, EventArgs e) => LoadDuration();
+
+        private void edCaptions_TextChanged(object sender, EventArgs e) => CaptionsChanged();
+
+        private void edLyrics_TextChanged(object sender, EventArgs e) => LyricsChanged();
+
+        private void edVideoLength_ValueChanged(object sender, EventArgs e) => Recalculate();
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e) => e.Cancel = CancelClose(e.CloseReason);
+
+        #endregion
+
+        #region Private Methods
+
+        private bool CancelClose(CloseReason closeReason)
+        {
+            if (closeReason != CloseReason.UserClosing)
+                return false;
+            if (LyricsEdited)
+                switch (MessageBox.Show(
+                    this,
+                    "The lyrics have been edited. Save changes?",
+                    "Confirmation",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Warning))
+                {
+                    case DialogResult.Yes:
+                        SaveLyrics();
+                        break;
+                    case DialogResult.Cancel:
+                        return true;
+                }
+            if (CaptionsUpdated)
+                switch (MessageBox.Show(
+                    this,
+                    "The captions have been updated. Save changes?",
+                    "Confirmation",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Warning))
+                {
+                    case DialogResult.Yes:
+                        SaveCaptions();
+                        break;
+                    case DialogResult.Cancel:
+                        return true;
+                }
+            return false;
+        }
+
+        private void CaptionsChanged() => CaptionsUpdated = true;
+
+        private void LoadDuration()
+        {
+            if (VideoOpenDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                var file = TagLib.File.Create(VideoOpenDialog.FileName);
+                var duration = file.Properties.Duration;
+                edHours.Value = duration.Hours;
+                edMinutes.Value = duration.Minutes;
+                edSeconds.Value = duration.Seconds;
+            }
+        }
+
+        private void LoadLyrics()
+        {
+            if (LyricsOpenDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                using (var reader = new StreamReader(LyricsOpenDialog.FileName))
+                    LyricsTextBox.Text = reader.ReadToEnd();
+                LyricsEdited = false;
+            }
+        }
+
+        private void LyricsChanged()
+        {
+            LyricsEdited = true;
+            Recalculate();
+        }
+
+        private void Recalculate()
+        {
+            var lines = LyricsTextBox.Lines;
+            var output = Caption(lines, (int)edHours.Value, (int)edMinutes.Value, (int)edSeconds.Value);
+            CaptionsTextBox.Lines = output.ToArray();
+        }
+
+        private void SaveCaptions()
+        {
+            if (CaptionsSaveDialog.ShowDialog(this) == DialogResult.OK)
+                using (var writer = new StreamWriter(CaptionsSaveDialog.FileName))
+                {
+                    writer.Write(CaptionsTextBox.Text);
+                    writer.Flush();
+                    writer.Close();
+                }
+            CaptionsUpdated = false;
+        }
+
+        private void SaveLyrics()
+        {
+            if (LyricsSaveDialog.ShowDialog(this) == DialogResult.OK)
+                using (var writer = new StreamWriter(LyricsSaveDialog.FileName))
+                {
+                    writer.Write(LyricsTextBox.Text);
+                    writer.Flush();
+                    writer.Close();
+                }
+            LyricsEdited = false;
+        }
+
+        private void ToggleSplit() => SplitContainer.Orientation =
+            SplitContainer.Orientation == Orientation.Horizontal
+            ? Orientation.Vertical
+            : Orientation.Horizontal;
+
+        #endregion
+
+        #region Static Methods
+
+        private static IEnumerable<string> Caption(IEnumerable<string> lines, int hours, int minutes, int seconds) =>
+            Caption(lines, new TimeSpan(hours: hours, minutes: minutes, seconds: seconds));
+
+        private static IEnumerable<string> Caption(IEnumerable<string> lines, TimeSpan time)
+        {
+            var output = new List<string>();
+            int
+                lineCount = lines.Count(),
+                lineNumber = 1;
+            string
+                text = string.Empty,
+                start,
+                stop = "00:00:00,000";
+            var totalTime = time.TotalMilliseconds;
+            foreach (var line in lines)
+            {
+                start = stop;
+                stop = TimeSpan.FromMilliseconds(totalTime * lineNumber / lineCount).ToString(@"hh\:mm\:ss\,fff");
+                output.Add($"{lineNumber}");
+                output.Add($"{start} --> {stop}");
+                if (!string.IsNullOrWhiteSpace(line))
+                    text = line;
+                output.Add(text);
+                output.Add(string.Empty);
+                lineNumber++;
+            }
+            return output;
+        }
+
+        #endregion
+    }
+}
